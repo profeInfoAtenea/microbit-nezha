8c5bb80618107b7ba320c2c1e6f474e3b5567b6f
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2022-02-21 13:38:08 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+function girarDerecha () {
+    basic.showArrow(ArrowNames.East)
+    neZha.setMotorSpeed(neZha.MotorList.M1, 0)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 100)
+}
+function girarIzquierda () {
+    basic.showArrow(ArrowNames.West)
+    neZha.setMotorSpeed(neZha.MotorList.M1, 100)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 0)
+}
+function avanzar () {
+    basic.showArrow(ArrowNames.North)
+    neZha.setMotorSpeed(neZha.MotorList.M1, 100)
+    neZha.setMotorSpeed(neZha.MotorList.M4, -100)
+}
+function retroceder () {
+    basic.showArrow(ArrowNames.South)
+    neZha.setMotorSpeed(neZha.MotorList.M1, -100)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 100)
+}
 basic.forever(function () {
-	
+    if (10 >= PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)) {
+        retroceder()
+        girarIzquierda()
+    } else {
+        avanzar()
+    }
 })
